1fa22b0a2e536352d3aa95423f0d3393ec42c8e3
nnassar <nnassar>
2007-07-07 19:06:44 +0000

    testing

--- src/search.c
+++ src/search.c
@@ -184,8 +184,8 @@ int etymon_af_score_default(ETYMON_AF_SEARCH_STATE* state,
 		sumsq = 1.0;
 	}
 
-/*	printf("%d %d\n", idf[x], sumsq); */
 	for (x = 0; x < iresults_n; x++) {
+/*		printf(">> %f %f\n", idf[x], sumsq); */
 		iresults[x].score = (int)((idf[x] / sumsq) * 10000);
 	}
 
