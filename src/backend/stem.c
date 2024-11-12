c5e738055c301b76dd01544bc4ebde4f80898a74
Nassib Nassar <nrn@etymon.com>
2024-11-12 13:27:43 -0500

    Disable indentation warnings for stemmer

--- src/backend/stem.c
+++ src/backend/stem.c
@@ -3,5 +3,8 @@
 int afnop = 0;  /* placeholder to avoid compiler warnings */
 
 #ifdef HAVE_STEMMER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #include "porter.cc"
+#pragma GCC diagnostic pop
 #endif
